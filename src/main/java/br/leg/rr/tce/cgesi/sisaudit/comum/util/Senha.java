0b68db664418eea099e22cad2fe3761b9c11c13a
fpcarlos <fpcarlos@cgesint-45>
2016-04-26 13:24:15 -0400

    Projeto Sisaudit

---
+++ src/main/java/br/leg/rr/tce/cgesi/sisaudit/comum/util/Senha.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.leg.rr.tce.cgesi.sisaudit.comum.util;
+
+/**
+ *
+ * @author fpcarlos
+ */
+public class Senha {
+
+
+    public static void main(String[] args) {
+       
+    }
+
+}
